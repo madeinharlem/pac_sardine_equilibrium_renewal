66acfa929fe3da9f8b6792a6072e14300f49e082
madeinharlem <aneeshshankarh@gmail.com>
2017-12-22 07:00:26 -0800

    Added some music code while the for loops run!

--- sardine_equilibrium.R
+++ sardine_equilibrium.R
@@ -7,6 +7,42 @@
 #####(C) All parameter values below are cited with reference in the write-up
 
 ##########################################################Fixed parameters
+#install.packages("tuneR", repos = "http://cran.r-project.org")
+library(tuneR)
+#Functions
+sound_dist <- function(duration, samplingrate) {
+  #Speed of sound is 1125 ft/sec
+  return((duration/samplingrate)*1125/2)
+}
+sound_data <- function(dataset, threshold, samplingrate) {
+  dataset <- snap@left
+  threshold = 4000
+  samplingrate = 44100
+  data <- data.frame()
+  max = 0
+  maxindex = 0
+  for (i in 1:length(dataset)) {
+    if (dataset[i] > max) {
+      max = dataset[i]
+      maxindex = i
+      data <- data.frame()
+    }
+    if (abs(dataset[i]) > threshold) {
+      data <- rbind(data, c(i,dataset[i], sound_dist(i - maxindex, samplingrate)))
+    }
+  }
+  colnames(data) <- c("x", "y", "dist")
+  return(data)
+}
+#Analysis
+snap <- readWave("2052341246920171222143858 (1).wav")
+print(snap)
+play(snap)
+plot(snap@left[30700:31500], type = "l", main = "Snap",
+     xlab = "Time", ylab = "Frequency")
+data <- sound_data(snap@left, 4000, 44100)
+plot(data[,3], data[,2], type = "l", main = "Snap",
+     xlab = "Dist", ylab = "Frequency")
 
 S_a<-1 #Selectivity at age - Knife edged
 Linf<-23.4 # Asymptotic length
